092b7084f359724a41d1245918da1525e8e0b453
HeinzBonen <244633@edu.rocfriesepoort.nl>
2024-11-19 13:16:24 +0100

    base html pages

---
+++ website/pages/cpulvlselect.php
@@ -0,0 +1,19 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+
+<form action="lvlSelectAction.php" method="POST">
+    <button type="submit" value="easy">makkelijk</button><br />
+    <button type="submit" value="normal">normaal</button><br />
+    <button type="submit" value="hard">moeilijk</button>
+</form>
+
+</body>
+</html>
---
+++ website/pages/index.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<form action="startAction.php" method="POST">
+    <button type="submit" value="cpu">tegen cpu</button><br />
+    <button type="submit" value="player">tegen speler</button><br />
+    <button type="submit" value="history">geschiedenis</button>
+</form>
+
+</body>
+</html>
